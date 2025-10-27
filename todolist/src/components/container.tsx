29eadde2df6d846fb18ebc2aa37e90f19298e299
Vitor Levi <levivitor8@gmail.com>
2025-10-27 15:12:31 -0300

    feat(components): create the container component

---
+++ todolist/src/components/container.tsx
@@ -0,0 +1,23 @@
+import { cva, type VariantProps } from "class-variance-authority"
+import React from "react"
+
+export const containerVariants = cva("mx-auto ", {
+  variants: {
+    size: {
+      md: "max-w[31.5rem] px-2"
+    }
+  },
+  defaultVariants: {
+    size: "md"
+  }
+})
+
+interface CardProps extends VariantProps<typeof containerVariants>, React.ComponentProps<"div"> {
+  as?: keyof React.JSX.IntrinsicElements
+}
+
+export default function Card({ as = "div", size, className, children, ...props }: CardProps) {
+  return React.createElement(
+    as, { className: containerVariants({ size, className }), ...props }, children
+  )
+}
