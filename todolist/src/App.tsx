fac3352f506262d07c76b41d7f0025e6fd511e62
Vitor Levi <levivitor8@gmail.com>
2025-10-27 10:37:08 -0300

    feat(components): create the badge component

--- todolist/src/App.tsx
+++ todolist/src/App.tsx
@@ -1,13 +1,18 @@
 import TrashIcon from "./assets/icons/trash.svg?react";
-import Icon from "./components/icon";
+import Icon from "./icon";
 import { Text } from "./components/text";
-
+import Badge from "./components/badge";
 export default function App() {
   return (
     <>
       <Text as="div" variant={"body-md"} className="text-pink-base">
         <Icon svg={TrashIcon} className="w-6 h-6 fill-current" />
       </Text>
+
+      <div>
+        <Badge variant={"secundary"}>5</Badge>
+        <Badge variant={"primary"}>2 de 5</Badge>
+      </div>
     </>
   )
 }
---
+++ todolist/src/components/badge.tsx
@@ -0,0 +1,41 @@
+/* eslint-disable react-refresh/only-export-components */
+import { Text } from "./text";
+import React from "react";
+import { cva, type VariantProps } from "class-variance-authority";
+
+export const badgeVariants = cva("inline-flex items-center justify-center rounded-full", {
+  variants: {
+    variant: {
+      primary: "bg-green-light",
+      secundary: "bg-pink-light",
+    },
+    size: {
+      sm: "py-0.5 px-2"
+    },
+  },
+  defaultVariants: {
+    variant: "primary",
+    size: "sm"
+  }
+})
+export const textVariants = cva("", {
+  variants: {
+    variant: {
+      primary: "text-green-dark",
+      secundary: "text-pink-dark"
+    }
+  },
+  defaultVariants: {
+    variant: "primary"
+  }
+})
+interface BadgeProps extends React.ComponentProps<"div">, VariantProps<typeof badgeVariants> {
+
+}
+export default function Badge({ variant, size, children, className, ...props }: BadgeProps) {
+  return (
+    <div className={badgeVariants({ variant, size, className })} {...props} >
+      <Text variant={"body-md-bold"} className={textVariants({ variant })}>{children}</Text>
+    </div>
+  )
+}
