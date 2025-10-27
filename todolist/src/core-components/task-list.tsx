33506ab1311f401fb640d6106835f3587b0c271a
Vitor Levi <levivitor8@gmail.com>
2025-10-27 20:55:57 -0300

    feat(hook): create the hook useTasks

--- todolist/src/core-components/task-list.tsx
+++ todolist/src/core-components/task-list.tsx
@@ -1,9 +1,10 @@
 import PlusIcon from "../assets/icons/more.svg?react";
 import Button from "../components/button";
+import useTasks from "../hooks/useTasks";
 import TaskItem from "./task-item";
 
 export default function TaskList() {
-
+  const { tasks } = useTasks()
   return (
     <>
       <Button variant={"primary"} className="w-full rounded-2xl" icon={PlusIcon}>
