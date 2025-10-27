d5eb7b7f395893ad4e95de031f148952e838a8c3
Vitor Levi <levivitor8@gmail.com>
2025-10-27 15:03:52 -0300

    efeat(components): create the card component

--- todolist/src/components/button-icon.tsx
+++ todolist/src/components/button-icon.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react-refresh/only-export-components */
 import type React from "react";
 import Icon from "./icon";
 import { cva, type VariantProps } from "class-variance-authority"
